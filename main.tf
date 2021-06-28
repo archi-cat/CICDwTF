ca382e646d0b680bb749fdc5b9a0e0c53818fdde
Archicat <archi_cat@yahoo.com>
2021-06-28 12:41:53 +0100

    removed backend from main.tf

--- main.tf
+++ main.tf
@@ -1,10 +1,3 @@
-  backend "azurerm" {
-    resource_group_name = "tf-tfstate"
-    storage_account_name = "tfstateblobacc1"
-    container_name = "tfstate"
-    key = "terraform.tfstate"
-  }
-
 provider "azurerm" {  
   features {}
 }
