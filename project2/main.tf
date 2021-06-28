6f18501514b66d9b8d20688ac94c194b18c4fb8f
Archicat <archi_cat@yahoo.com>
2021-06-28 20:10:50 +0100

    1st time add tf files

---
+++ project2/main.tf
@@ -0,0 +1,8 @@
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "t2a-rg" {
+    name = var.rg_name
+    location = var.rg_location  
+}
---
+++ project2/variables.tf
@@ -0,0 +1,12 @@
+variable "rg_name" {
+  type = string
+  description = "Name of the resource group"
+  default = "t2a-rg"
+}
+
+variable "rg_location" {
+  type = string
+  description = "Location of the resource group"
+  default = "uksouth"
+}
+
