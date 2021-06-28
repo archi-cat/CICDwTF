d2bd514605bde4990740c3b9156c50ec114ff2ac
Archicat <archi_cat@yahoo.com>
2021-06-28 21:05:53 +0100

    Added required providers to main.tf

--- project2/main.tf
+++ project2/main.tf
@@ -1,3 +1,15 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "2.65.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features = {}
+}
 resource "azurerm_resource_group" "t2a-rg" {
     name = var.rg_name
     location = var.rg_location  
